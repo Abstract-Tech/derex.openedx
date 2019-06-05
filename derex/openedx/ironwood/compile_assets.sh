fe7ee12dadd1e72bd80daa88c9c42131f5948b4e
Silvio Tomatis <silviot@gmail.com>
2019-06-05 14:31:02 +0200

    Don't use nodeenv to invoke npm/node

--- derex/openedx/ironwood/compile_assets.sh
+++ derex/openedx/ironwood/compile_assets.sh
@@ -2,14 +2,14 @@
 set -e
 set -x
 
-apk add nodejs
-nodeenv /openedx/nodeenv --node=10.16.0 --prebuilt
+apk add nodejs-npm alpine-sdk
 
 cd /openedx/edx-platform
-/openedx/nodeenv/bin/npm set progress=false
-/openedx/nodeenv/bin/npm install
-echo PATH=/openedx/edx-platform/node_modules/.bin:/openedx/nodeenv/bin:/openedx/bin:\$\{PATH\}>>~/.profile
-PATH=/openedx/edx-platform/node_modules/.bin:/openedx/nodeenv/bin:/openedx/bin:${PATH}
+npm install -g npm
+npm set progress=false
+npm install
+echo PATH=/openedx/edx-platform/node_modules/.bin:/openedx/bin:\$\{PATH\}>>~/.profile
+PATH=/openedx/edx-platform/node_modules/.bin:/openedx/bin:${PATH}
 
 cd /openedx/edx-platform
 openedx-assets xmodule
@@ -24,14 +24,13 @@ openedx-assets collect --settings=derex.assets
 echo Freeing up some space. Before:
 du / -sch
 rm -r \
-    /openedx/nodeenv/ `# 137.3M` \
     /openedx/edx-platform/node_modules/ `# 368.9M` \
     /usr/lib/node_modules/ `# 30.4M` \
     /openedx/staticfiles/studio-frontend/node_modules `# 24.3M` \
     /openedx/staticfiles/cookie-policy-banner/node_modules `# 5.7M` \
     /openedx/staticfiles/edx-bootstrap/node_modules `# 10.9M` \
     /openedx/staticfiles/paragon/node_modules `# 13.3M`
-apk del nodejs # 52M
+apk del nodejs-npm alpine-sdk # 100M
 
 echo After:
 du / -sch
