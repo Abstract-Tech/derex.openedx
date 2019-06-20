9f95d8e8e388e3f2e714c80ab66e0d9f71ea3084
Silvio Tomatis <silviot@gmail.com>
2019-06-20 17:09:56 +0200

    Add users fixtures

--- derex/openedx/ironwood/install_custom.sh
+++ derex/openedx/ironwood/install_custom.sh
@@ -6,3 +6,5 @@ mkdir /openedx/edx-platform/lms/envs/derex
 mv /tmp/base.py /openedx/edx-platform/lms/envs/derex/
 ln -s /openedx/edx-platform/lms/envs/derex/base.py /openedx/edx-platform/cms/envs/derex/
 mv /tmp/restore_dump.py /openedx/bin/
+
+mv /tmp/fixtures /openedx/
