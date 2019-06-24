85badecd469b6c84f722f1ecba40b145c9305c74
Silvio Tomatis <silviot@gmail.com>
2019-06-25 00:04:17 +0200

    Do not remove paragon from themes

--- derex/openedx/ironwood/prepare_assets.sh
+++ derex/openedx/ironwood/prepare_assets.sh
@@ -3,7 +3,7 @@ set -ex
 
 PATH=/openedx/edx-platform/node_modules/.bin:/openedx/nodeenv/bin:/openedx/bin:${PATH}
 
-rm -rf /openedx/edx-platform/themes/*
+rm -rvf /openedx/edx-platform/themes/[!p]*
 compile_assets.sh
 cleanup_assets.sh
 symlink_duplicates.py /openedx/staticfiles
