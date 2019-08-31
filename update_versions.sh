133b4d7e8396ab7574d8347e67a81900d2088ded
Silvio Tomatis <silviot@gmail.com>
2019-08-31 17:06:46 +0200

    Fix scripts and requirements (again, hopefully)

--- update_versions.sh
+++ update_versions.sh
@@ -3,39 +3,38 @@ EDX_PLATFORM_REPOSITORY=https://github.com/edx/edx-platform.git
 EDX_PLATFORM_VERSION=open-release/ironwood.master
 
 docker run --rm "$(derex.builder image derex/openedx/buildwheels/)" sh -c "
-pip install -q pip==18.1 2> /dev/null
-git clone -q ${EDX_PLATFORM_REPOSITORY} --branch ${EDX_PLATFORM_VERSION} --depth 1 /openedx/edx-platform 2> /dev/null
+pip install pip==18.1 >&2
+git clone ${EDX_PLATFORM_REPOSITORY} --branch ${EDX_PLATFORM_VERSION} --depth 1 /openedx/edx-platform >&2
 cd /openedx/edx-platform
 
 # pip-sync is confused by the latest version of matplotlib being only compatible with python 3
-echo matplotlib==1.5.3 >> /openedx/edx-platform/requirements/constraints.txt
+#echo 'matplotlib<3.0.0' >> requirements/edx/base.in
+echo 'matplotlib==1.5.3' >> requirements/edx/base.in
+
 
 # AAARGHHH! WHY IS THIS FAILING, AND THIS NECESSARY??!!
-#echo edx-user-state-client==1.0.4 >> /openedx/edx-platform/requirements/constraints.txt
-#echo edx-ccx-keys==0.2.1 >> /openedx/edx-platform/requirements/constraints.txt
-
+# Pinning versions that would otherwise result in incompatibilities
+sed /edx-user-state-client/d -i requirements/edx/paver.in
+echo 'edx-user-state-client<=1.0.4' >> requirements/edx/base.in
+sed /edx-ccx-keys/d -i requirements/edx/paver.in
+echo 'edx-ccx-keys<=0.2.1' >> requirements/edx/base.in
+sed /edx-opaque-keys/d -i requirements/edx/paver.in
+echo 'edx-opaque-keys<1.0.0' >> requirements/edx/paver.in
 # We don't need the testing or development versions
-#sed /requirements.edx.testing/d -i Makefile
-#sed /requirements.edx.development/d -i Makefile
+sed /requirements.edx.testing/d -i Makefile
+sed /requirements.edx.development/d -i Makefile
 
-# xmodule requires a version of edx-opaque-keys<1.0.0
-# and messes with edx-ccx-keys version requirements
-# that requires edx-opaque-keys>=1.0.1,<2.0.0
-# It's being discontinued, so we omit it
-sed /common.lib.xmodule/d -i requirements/edx/local.in
+apk add bash >&2
 
-# Isn't base.txt supposed to be about to be regenerated?
-# Why is it being looked into?
-sed /python-slugify/d -i requirements/edx/base.txt
-
-apk add bash -q
-
-make upgrade > /dev/null 2> /dev/null
-# The first time it uses an older version of pip-tools
-# and errors out
-make upgrade > /dev/null 2> /dev/null
+# Upgrade pip-tools befor running make
+pip install -r requirements/edx/pip-tools.txt >&2
+pip-compile -v --no-emit-trusted-host --no-index --upgrade -o requirements/edx/pip-tools.txt requirements/edx/pip-tools.in
+# Install our numpy version. Otherwise 'python setup.py egg_info' (run by pip-compile)
+# fails for matplotlib
+pip install $(grep numpy  requirements/edx/base.in|awk '{print $1}') >&2
+make upgrade >&2
 sed -e s@file:///openedx/edx-platform/@@ -i requirements/edx/*.txt
-sed -e s@file:///openedx/edx-platform@.@ -i requirements/edx/*.txt
+sed -e 's@-e git+https://@git+https://@' -i requirements/edx/*.txt
 cat requirements/edx-sandbox/base.txt
 " > derex/openedx/ironwood/requirements.txt
 grep -v ^-e derex/openedx/ironwood/requirements.txt > derex/openedx/wheels/requirements.txt
