be0a4a4fcf744d7fba50c4cf6939a6bef77b3026
Silvio Tomatis <silviot@gmail.com>
2019-09-03 16:52:33 +0200

    Fix KeyError: u'cornerstone' error during assets compilation

--- derex/openedx/ironwood/assets.py
+++ derex/openedx/ironwood/assets.py
@@ -2,12 +2,12 @@
 Bare minimum settings for collecting production assets.
 """
 
-import os
-
+from ..common import *  # type: ignore
 from openedx.core.lib.derived import derive_settings
 from path import Path as path
 
-from ..common import *  # type: ignore
+import os
+
 
 COMPREHENSIVE_THEME_DIRS.append("/openedx/themes")  # type: ignore
 STATIC_ROOT_BASE = "/openedx/staticfiles"
@@ -22,4 +22,9 @@ WEBPACK_LOADER["DEFAULT"]["STATS_FILE"] = (  # type: ignore
 SECRET_KEY = "secret"
 XQUEUE_INTERFACE = {"django_auth": None, "url": None}
 DATABASES = {"default": {}}  # type: ignore
+
+# Prevent KeyError: u'cornerstone' error in simple_history/models:212
+# https://github.com/treyhunner/django-simple-history/blob/b1d9adbd838836246b052b4c9c4598e02f6471c5/simple_history/models.py#L213
+INSTALLED_APPS.append("integrated_channels.cornerstone")
+
 derive_settings(__name__)
