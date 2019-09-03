aaa6641e378bb45972f8855f0e6147aa455f50ea
Silvio Tomatis <silviot@gmail.com>
2019-09-03 15:01:20 +0200

    Fix matplotlib issue

--- update_versions.sh
+++ update_versions.sh
@@ -2,8 +2,8 @@
 EDX_PLATFORM_REPOSITORY=https://github.com/edx/edx-platform.git
 EDX_PLATFORM_VERSION=open-release/ironwood.master
 
-if [ ! -z "$PIP_CACHE" ]; then
-    PIP_DOCKER_OPTION="-v $PIP_CACHE:/root/.cache/pip"
+if [ ! -z "$PIP_TOOLS_CACHE" ]; then
+    PIP_DOCKER_OPTION="-v $PIP_TOOLS_CACHE:/root/.cache/pip-tools"
 fi
 # shellcheck disable=SC2086
 docker run $PIP_DOCKER_OPTION --rm "$(derex.builder image derex/openedx/buildwheels/)" sh -c "
@@ -11,9 +11,9 @@ pip install pip==18.1 >&2
 git clone ${EDX_PLATFORM_REPOSITORY} --branch ${EDX_PLATFORM_VERSION} --depth 1 /openedx/edx-platform >&2
 cd /openedx/edx-platform
 
-# pip-sync is confused by the latest version of matplotlib being only compatible with python 3
-echo 'matplotlib<3.0.0' >> requirements/edx/base.in
-echo 'matplotlib<3.0.0' >> requirements/constraints.txt
+# Copy the edx-sandbox matplotlib spec to edx/base.in
+grep matplotlib requirements/edx-sandbox/base.in >> requirements/edx/base.in
+grep matplotlib requirements/edx-sandbox/base.in >> requirements/constraints.txt
 
 # These versions need to be pinned, otherwise they pull in an
 # incompatible version of edx-opaque-keys
@@ -24,21 +24,18 @@ sed -i 's/edx-opaque-keys.*/edx-opaque-keys<1.0.0/' requirements/edx/base.in
 sed -i 's/edx-opaque-keys.*/edx-opaque-keys<1.0.0/' requirements/edx/paver.in
 sed -i 's/edx-milestones.*/edx-milestones<0.2.3/' requirements/edx/base.in
 sed -i 's/edx-organizations.*/edx-organizations<2.1.0/' requirements/edx/base.in
-edx-milestones
+
 # We don't need the testing or development versions, so we can speed up the
 # process by removing them
 sed /requirements.edx.testing/d -i Makefile
 sed /requirements.edx.development/d -i Makefile
 
+# scripts/post-pip-compile.sh specifies bash in its shebang. Make sure it's present
 apk add bash >&2
 
 # Upgrade pip-tools befor running make
 pip install -r requirements/edx/pip-tools.txt >&2
 pip-compile -v --no-emit-trusted-host --no-index --upgrade -o requirements/edx/pip-tools.txt requirements/edx/pip-tools.in >&2
-
-# Install our numpy version. Otherwise 'python setup.py egg_info' (run by pip-compile)
-# fails for matplotlib
-pip install \$(grep numpy requirements/edx/base.in|awk '{print $1}') >&2
 
 # Document this command to update versions
 export CUSTOM_COMPILE_COMMAND='./update_versions.sh'
@@ -48,7 +45,6 @@ make upgrade >&2
 
 sed -e s@file:///openedx/edx-platform/@@ -i requirements/edx/*.txt
 sed -e s@file:///openedx/edx-platform@.@ -i requirements/edx/*.txt
-sed -e 's@-e git+https://@git+https://@' -i requirements/edx/*.txt
 cat requirements/edx-sandbox/base.txt
 " > derex/openedx/ironwood/requirements.txt
 
