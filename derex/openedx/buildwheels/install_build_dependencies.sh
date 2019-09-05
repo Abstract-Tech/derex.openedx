55738a7a70b9abdaff054641a7c57c068d6bfd9b
Silvio Tomatis <silviot@gmail.com>
2019-09-05 23:27:17 +0200

    Fix mysql-python installation

--- derex/openedx/buildwheels/install_build_dependencies.sh
+++ derex/openedx/buildwheels/install_build_dependencies.sh
@@ -23,8 +23,12 @@ apk add \
     linux-headers \
     make \
     mariadb-connector-c-dev \
+    mariadb-dev \
     pkgconfig \
     python-dev \
     sqlite-dev \
     swig \
     xmlsec-dev
+
+# From https://github.com/jfloff/alpine-python/issues/32
+sed '/st_mysql_options options;/a unsigned int reconnect;' /usr/include/mysql/mysql.h -i
