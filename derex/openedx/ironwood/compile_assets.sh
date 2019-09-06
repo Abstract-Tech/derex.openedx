efce3ab9dc4282b7dd54e3d43cc54e3feb41c5ac
Silvio Tomatis <silviot@gmail.com>
2019-09-07 01:46:23 +0200

    No point in --delete to save space in a layer environment like docker

--- derex/openedx/ironwood/compile_assets.sh
+++ derex/openedx/ironwood/compile_assets.sh
@@ -34,7 +34,7 @@ symlink_duplicates.py "${STATIC_ROOT_LMS}"
 
 echo Rsync assets in place to avoid replacing the same file
 # Careful to include a trailing slash for the source dir: rsync is sensitive to this
-rsync --delete -a "${STATIC_ROOT_LMS}"/ "${FINAL_STATIC_ROOT}"
+rsync -a "${STATIC_ROOT_LMS}"/ "${FINAL_STATIC_ROOT}"
 
 # Cleanup is done in cleanup_assets.sh, but we remove /tmp/staticfiles here
 rm -rf /tmp/staticfiles
