5e77a274ba2bfe73123970b5da109b92c1769a42
Silvio Tomatis <silviot@gmail.com>
2019-09-06 19:25:01 +0200

    Try using paver to compile assets

--- derex/openedx/ironwood/compile_assets.sh
+++ derex/openedx/ironwood/compile_assets.sh
@@ -20,38 +20,9 @@ npm install
 grep -q ^export\ PATH=/openedx /etc/profile || echo export PATH=/openedx/edx-platform/node_modules/.bin:/openedx/bin:\$\{PATH\}>>/etc/profile
 PATH=/openedx/edx-platform/node_modules/.bin:/openedx/bin:${PATH}
 
-python -c "
-import sys
-sys.argv[1:] = ['common/static/xmodule']
-from xmodule import static_content as xmodule_static_content
-print('Compiling xmodules')
-xmodule_static_content.main()
-from pavelib import assets
-print('Processing npm assets')
-assets.process_npm_assets()
-"
-webpack --config=webpack.prod.config.js
-python -c "
-from pavelib import assets
-assets._compile_sass('lms', None, False, False, [])
-assets._compile_sass('cms', None, False, False, [])
-"
-python -c "
-from path import Path as path
-from pavelib import assets
-import os
-
-THEME_DIR = os.environ.get('THEME_DIR')
-for theme in path(THEME_DIR).listdir():
-    if theme.basename().startswith('.'):
-        continue
-    for system in ('lms', 'cms'):
-        print('Compiling theme {} ({})'.format(theme.basename(), system))
-        assets._compile_sass('lms', theme, False, False, [])
-"
-echo Collecting assets
-python manage.py lms --settings=derex.assets collectstatic --ignore "fixtures" --ignore "karma_*.js" --ignore "spec" --ignore "spec_helpers" --ignore "spec-helpers" --ignore "xmodule_js" --ignore "geoip" --ignore "sass" --noinput
-python manage.py cms --settings=derex.assets collectstatic --ignore "fixtures" --ignore "karma_*.js" --ignore "spec" --ignore "spec_helpers" --ignore "spec-helpers" --ignore "xmodule_js" --ignore "geoip" --ignore "sass" --noinput
+export NO_PREREQ_INSTALL=True
+export NO_PYTHON_UNINSTALL=True
+paver update_assets --settings derex.assets
 
 echo Rsync assets in place to avoid replacing the same file
 apk add rsync --no-cache
