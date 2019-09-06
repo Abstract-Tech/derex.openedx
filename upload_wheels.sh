0f8d0da1d11a9dd9a35649edfea1752b0038a29a
Silvio Tomatis <silviot@gmail.com>
2019-09-06 13:10:02 +0200

    A flat directory layout is ok

--- upload_wheels.sh
+++ upload_wheels.sh
@@ -11,9 +11,7 @@ BASE_DIR=alpine-3.10
 wheel_files=$(find /var/cache/pip-alpine|grep whl$)
 for filepath in $wheel_files; do
     filename=$(basename "$filepath")
-    # shellcheck disable=SC2001
-    package_name=$(echo "$filename"|sed s/-.*//)
-    server_path="$BASE_DIR/$package_name/$filename"
+    server_path="${BASE_DIR}/${filename}"
     if ! curl -s -I "${DNS_NAME}${server_path}" | grep -q "HTTP/1.1 200"; then
         swift upload --object-name "${server_path}" "${CONTAINER_NAME}" "${filepath}"
         echo "${filename}" uploaded
