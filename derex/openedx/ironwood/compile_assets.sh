1b003acfae14645dffb3cb77a6d0ce05fe7c628f
Silvio Tomatis <silviot@gmail.com>
2019-09-09 00:48:24 +0200

    Stop using rsync

--- derex/openedx/ironwood/compile_assets.sh
+++ derex/openedx/ironwood/compile_assets.sh
@@ -2,15 +2,13 @@
 set -e
 set -x
 
-export FINAL_STATIC_ROOT="/openedx/staticfiles"
-export STATIC_ROOT_LMS="/tmp/staticfiles"
+export STATIC_ROOT_LMS="/openedx/staticfiles"
 export STATIC_ROOT_CMS=${STATIC_ROOT_LMS}/studio
 export THEME_DIR="/openedx/themes"
 export NODE_ENV=${NODE_ENV:-production}
 
 [ -x /usr/bin/node ] || apk add nodejs --no-cache
 [ -x /usr/bin/npm ] || apk add npm --no-cache
-[ -x /usr/bin/rsync ] || apk add rsync --no-cache
 [ -x /usr/bin/make ] || apk add make --no-cache
 [ -x /usr/bin/g++ ] || apk add g++ --no-cache
 
@@ -26,11 +24,6 @@ paver update_assets --settings derex.assets --themes open-edx "$1"
 
 echo Symlinking files with the same content
 symlink_duplicates.py "${STATIC_ROOT_LMS}"
-
-echo Rsync assets in place to avoid replacing the same file
-# Careful to include a trailing slash for the source dir: rsync is sensitive to this
-# -a is equivalento to -rlptgoD, but we want to omit ptgo and are not interested in D
-rsync -rl "${STATIC_ROOT_LMS}"/ "${FINAL_STATIC_ROOT}"
 
 # Cleanup is done in cleanup_assets.sh, but we remove /tmp/staticfiles here
 rm -rf /tmp/staticfiles
