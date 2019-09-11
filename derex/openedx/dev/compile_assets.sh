f107f38814f09f79760e7d3d57547339a37c2cca
Silvio Tomatis <silviot@gmail.com>
2019-09-12 00:19:52 +0200

    Clean up

--- derex/openedx/dev/compile_assets.sh
+++ derex/openedx/dev/compile_assets.sh
@@ -7,11 +7,6 @@ export STATIC_ROOT_CMS=${STATIC_ROOT_LMS}/studio
 export THEME_DIR="/openedx/themes"
 export NODE_ENV=${NODE_ENV:-production}
 
-[ -x /usr/bin/node ] || apk add nodejs --no-cache
-[ -x /usr/bin/npm ] || apk add npm --no-cache
-[ -x /usr/bin/make ] || apk add make --no-cache
-[ -x /usr/bin/g++ ] || apk add g++ --no-cache
-
 cd /openedx/edx-platform
 npm set progress=false
 npm install
@@ -24,6 +19,3 @@ paver update_assets --settings derex.assets --themes open-edx "$1"
 
 echo Symlinking files with the same content
 symlink_duplicates.py "${STATIC_ROOT_LMS}"
-
-# Cleanup is done in cleanup_assets.sh, but we remove /tmp/staticfiles here
-rm -rf /tmp/staticfiles
