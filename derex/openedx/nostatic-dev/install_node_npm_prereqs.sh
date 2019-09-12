97936647ab4a7e802ad2bed5b207c49db6f6f9e4
Silvio Tomatis <silviot@gmail.com>
2019-09-12 23:05:06 +0200

    Also install coreutils

--- derex/openedx/nostatic-dev/install_node_npm_prereqs.sh
+++ derex/openedx/nostatic-dev/install_node_npm_prereqs.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
-apk add npm nodejs g++ make
+# rmlint's shell script uses features of mktemp not present in busybox
+# hence we install coreutils
+apk add npm nodejs g++ make coreutils
 cd /openedx/edx-platform
 npm install
