2bd11b559b2a067a7d91d045d526948e91ca5af9
Silvio Tomatis <silviot@gmail.com>
2019-09-14 01:33:01 +0200

    Deduplicate node_modules early

--- derex/openedx/nostatic-dev/install_node_npm_prereqs.sh
+++ derex/openedx/nostatic-dev/install_node_npm_prereqs.sh
@@ -6,3 +6,10 @@ set -ex
 apk add npm nodejs g++ make coreutils
 cd /openedx/edx-platform
 npm install
+
+# Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
+# and symlinks the other copies
+rmlint -g -D node_modules
+# Do not remove empty files/directories
+sed "/# empty /d" -i rmlint.sh
+./rmlint.sh -d
