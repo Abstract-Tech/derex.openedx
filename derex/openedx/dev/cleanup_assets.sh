4ff802e1bfd6fcdc71dd72faedf60e2bf5b6a6bf
Silvio Tomatis <silviot@gmail.com>
2019-09-14 01:40:25 +0200

    Make rmlint action more specific

--- derex/openedx/dev/cleanup_assets.sh
+++ derex/openedx/dev/cleanup_assets.sh
@@ -3,14 +3,14 @@ set -e
 set -x
 
 echo Freeing up some space. Before:
-du /openedx -sch
+du /openedx/staticfiles /openedx/edx-platform/node_modules -sch
 
 # Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
 # and symlinks the other copies
-rmlint -g -D /openedx
+rmlint -g -D /openedx/staticfiles /openedx/edx-platform/node_modules
 # Do not remove empty files/directories
 sed "/# empty /d" -i rmlint.sh
 ./rmlint.sh -d
 
 echo After:
-du /openedx -sch
+du /openedx/staticfiles /openedx/edx-platform/node_modules -sch
