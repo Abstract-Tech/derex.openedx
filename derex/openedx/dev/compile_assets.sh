a4ea409b41485f1e28138e9fff5b17a91e7549ee
Silvio Tomatis <silviot@gmail.com>
2019-09-13 14:06:36 +0200

    compile_assets.sh now expects all node packages already in place

--- derex/openedx/dev/compile_assets.sh
+++ derex/openedx/dev/compile_assets.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-set -e
-set -x
+set -ex
 
 export STATIC_ROOT_LMS="/openedx/staticfiles"
 export STATIC_ROOT_CMS=${STATIC_ROOT_LMS}/studio
 export THEME_DIR="/openedx/themes"
 export NODE_ENV=${NODE_ENV:-production}
 
-cd /openedx/edx-platform
-npm set progress=false
-npm install --no-cache
-
 export NO_PREREQ_INSTALL=True
 export NO_PYTHON_UNINSTALL=True
+cd /openedx/edx-platform
 paver update_assets --settings derex.assets --themes open-edx "$1"
--- derex/openedx/dev/install_dev_goodies.sh
+++ derex/openedx/dev/install_dev_goodies.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+set -ex
+
 ln -sf /openedx/edx-platform/lms/envs/derex/base.py /openedx/edx-platform/cms/envs/derex/
 
 pip install pdbpp ipython
-apk add vim --no-cache
+apk add vim
+
+cd /openedx/edx-platform
+npm set progress=false
+npm install
