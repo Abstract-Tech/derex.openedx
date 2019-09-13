6403bbbbc80a15dcd01de7a9ee5d29bc94976378
Silvio Tomatis <silviot@gmail.com>
2019-09-14 01:55:36 +0200

    Fix rmlint invocation

--- derex/openedx/dev/cleanup_assets.sh
+++ derex/openedx/dev/cleanup_assets.sh
@@ -3,14 +3,14 @@ set -e
 set -x
 
 echo Freeing up some space. Before:
-du /openedx/staticfiles /openedx/edx-platform/node_modules -sch
+du /openedx -sch
 
 # Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
 # and symlinks the other copies
-rmlint -g -D /openedx/staticfiles /openedx/edx-platform/node_modules
+rmlint -o sh:rmlint.sh -c sh:symlink -g /openedx/
 # Do not remove empty files/directories
 sed "/# empty /d" -i rmlint.sh
 ./rmlint.sh -d
 
 echo After:
-du /openedx/staticfiles /openedx/edx-platform/node_modules -sch
+du /openedx -sch
--- derex/openedx/nostatic-dev/install_node_npm_prereqs.sh
+++ derex/openedx/nostatic-dev/install_node_npm_prereqs.sh
@@ -9,7 +9,7 @@ npm install
 
 # Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
 # and symlinks the other copies
-rmlint -g -D node_modules
+rmlint -o sh:rmlint.sh -c sh:symlink -g node_modules
 # Do not remove empty files/directories
 sed "/# empty /d" -i rmlint.sh
 ./rmlint.sh -d
