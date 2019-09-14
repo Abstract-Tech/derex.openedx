3bcf8151e248845d4265c1d76cc89557462f9d3a
Silvio Tomatis <silviot@gmail.com>
2019-09-14 14:05:42 +0200

    Run rmlint across static files dirs

--- derex/openedx/dev/cleanup_assets.sh
+++ derex/openedx/dev/cleanup_assets.sh
@@ -4,7 +4,12 @@ set -x
 
 # Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
 # and symlinks the other copies
-rmlint -o sh:rmlint.sh -c sh:symlink -o json:stderr -g /openedx/staticfiles 2>/dev/null
+rmlint -o sh:rmlint.sh -c sh:symlink -o json:stderr -g \
+    /openedx/staticfiles \
+    /openedx/edx-platform/common/static/ \
+    /openedx/edx-platform/lms/static/ \
+    /openedx/edx-platform/cms/static/ \
+    2>/dev/null
 # Do not remove empty files/directories
 sed "/# empty /d" -i rmlint.sh
 ./rmlint.sh -d
