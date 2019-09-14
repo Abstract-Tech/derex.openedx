f49a3e12a294c9cffe4edd999aea1c6e97de8285
Silvio Tomatis <silviot@gmail.com>
2019-09-15 01:23:09 +0200

    Try to rmlint the whole /openedx

--- derex/openedx/dev/cleanup_assets.sh
+++ derex/openedx/dev/cleanup_assets.sh
@@ -5,7 +5,7 @@ set -x
 # Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
 # and symlinks the other copies
 rmlint -o sh:rmlint.sh -c sh:symlink -o json:stderr -g \
-    /openedx/staticfiles \
+    /openedx \
     2>/dev/null
 # Do not remove empty files/directories
 sed "/# empty /d" -i rmlint.sh
