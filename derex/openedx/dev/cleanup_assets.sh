573a11c3d2b8297d0c3edf14499673dc01e52136
Silvio Tomatis <silviot@gmail.com>
2019-09-14 11:53:23 +0200

    Do not emit rmlint json

--- derex/openedx/dev/cleanup_assets.sh
+++ derex/openedx/dev/cleanup_assets.sh
@@ -4,7 +4,7 @@ set -x
 
 # Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
 # and symlinks the other copies
-rmlint -o sh:rmlint.sh -c sh:symlink -g /openedx/staticfiles
+rmlint -o sh:rmlint.sh -c sh:symlink -o json:stderr -g /openedx/staticfiles 2>/dev/null
 # Do not remove empty files/directories
 sed "/# empty /d" -i rmlint.sh
 ./rmlint.sh -d
