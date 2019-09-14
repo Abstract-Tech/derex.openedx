e9d9a748aae75ff91fcb4887ff712a7e200f8883
Silvio Tomatis <silviot@gmail.com>
2019-09-14 02:18:46 +0200

    Only rmlint staticfiles

--- derex/openedx/dev/cleanup_assets.sh
+++ derex/openedx/dev/cleanup_assets.sh
@@ -2,15 +2,9 @@
 set -e
 set -x
 
-echo Freeing up some space. Before:
-du /openedx -sch
-
 # Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
 # and symlinks the other copies
-rmlint -o sh:rmlint.sh -c sh:symlink -g /openedx/
+rmlint -o sh:rmlint.sh -c sh:symlink -g /openedx/staticfiles
 # Do not remove empty files/directories
 sed "/# empty /d" -i rmlint.sh
 ./rmlint.sh -d
-
-echo After:
-du /openedx -sch
--- derex/openedx/nostatic-dev/install_node_npm_prereqs.sh
+++ derex/openedx/nostatic-dev/install_node_npm_prereqs.sh
@@ -6,10 +6,3 @@ set -ex
 apk add npm nodejs g++ make coreutils
 cd /openedx/edx-platform
 npm install
-
-# Avoid dulicates: rmlint finds files with the same conents, keeps the oldest
-# and symlinks the other copies
-rmlint -o sh:rmlint.sh -c sh:symlink -g node_modules
-# Do not remove empty files/directories
-sed "/# empty /d" -i rmlint.sh
-./rmlint.sh -d
