bcbc896587b1cc98cdcbd7f61509f561d091fd15
Silvio Tomatis <silviot@gmail.com>
2019-09-14 15:24:18 +0200

    Only optimize /staticfiles

--- derex/openedx/dev/cleanup_assets.sh
+++ derex/openedx/dev/cleanup_assets.sh
@@ -6,9 +6,6 @@ set -x
 # and symlinks the other copies
 rmlint -o sh:rmlint.sh -c sh:symlink -o json:stderr -g \
     /openedx/staticfiles \
-    /openedx/edx-platform/common/static/ \
-    /openedx/edx-platform/lms/static/ \
-    /openedx/edx-platform/cms/static/ \
     2>/dev/null
 # Do not remove empty files/directories
 sed "/# empty /d" -i rmlint.sh
