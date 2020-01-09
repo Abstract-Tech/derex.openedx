fa7dea75bdfc2aed61da2306c48824fd824b8d64
Silvio Tomatis <silviot@gmail.com>
2020-01-09 12:59:13 +0100

    Remove cornerstone fix

--- derex/openedx/nostatic/assets.py
+++ derex/openedx/nostatic/assets.py
@@ -23,10 +23,6 @@ SECRET_KEY = "secret"
 XQUEUE_INTERFACE = {"django_auth": None, "url": None}
 DATABASES = {"default": {}}  # type: ignore
 
-# Prevent KeyError: u'cornerstone' error in simple_history/models:212
-# https://github.com/treyhunner/django-simple-history/blob/b1d9adbd838836246b052b4c9c4598e02f6471c5/simple_history/models.py#L213
-INSTALLED_APPS.append("integrated_channels.cornerstone")
-
 # enterprise.views tries to access settings.ECOMMERCE_PUBLIC_URL_ROOT,
 ECOMMERCE_PUBLIC_URL_ROOT = None
 derive_settings(__name__)
