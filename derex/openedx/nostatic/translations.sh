a8a2f6c55e3e66bc375f21a5ba239cf241529592
Silvio Tomatis <silviot@gmail.com>
2020-01-09 16:27:11 +0100

    Remove no longer needed fixes

--- derex/openedx/nostatic/translations.sh
+++ derex/openedx/nostatic/translations.sh
@@ -14,21 +14,8 @@ cd /openedx/edx-platform
 
 pip install transifex-client
 
-# Comment out broken languages (Chinese and Russian as of 11-06-2019)
-for lang in "zh_CN" "ru"
-do
-    sed -i -e "s/    - $lang/    # - $lang/" "conf/locale/config.yaml"
-done
-
 i18n_tool transifex pull
 i18n_tool extract
-
-# Fix broken plural expression header
-for file in "django-partial.po" "django-studio.po" "djangojs-partial.po" "djangojs-studio.po"
-do
-    sed -i -e "s/nplurals=INTEGER/nplurals=2/" "conf/locale/en/LC_MESSAGES/$file"
-    sed -i -e 's/plural=EXPRESSION/plural=\(n != 1\)/' "conf/locale/en/LC_MESSAGES/$file"
-done
 
 i18n_tool generate
 
