11155773f0da9c7b5e7b71c7c8b6b4d91ce3fe34
Silvio Tomatis <silviot@gmail.com>
2020-01-18 17:49:24 +0100

    Build the image

--- ci/compile_wheels.sh
+++ ci/compile_wheels.sh
@@ -3,6 +3,9 @@
 EDX_PLATFORM_REPOSITORY=${EDX_PLATFORM_REPOSITORY-https://github.com/edx/edx-platform.git}
 IMAGE=$(derex.builder image derex/openedx/buildwheels)
 
+echo "Building image (if necessary)"
+derex.builder resolve derex/openedx/buildwheels
+
 docker run -v "${PIP_CACHE}:/root/.cache/pip" -d --name derex.wheel.compiler --rm "$IMAGE" sleep 86400
 
 docker exec derex.wheel.compiler git clone "$EDX_PLATFORM_REPOSITORY" --branch "${EDX_PLATFORM_VERSION}" --depth 1
