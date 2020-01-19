14b000ec2791d78817aa42b8062c73b7a439585a
Silvio Tomatis <silviot@gmail.com>
2020-01-19 23:11:25 +0100

    Store the wheels in /tmp/wheelhouse and include Swift credentials

--- ci/compile_wheels.sh
+++ ci/compile_wheels.sh
@@ -5,7 +5,7 @@ set -ex
 EDX_PLATFORM_REPOSITORY=${EDX_PLATFORM_REPOSITORY-https://github.com/edx/edx-platform.git}
 IMAGE=$(derex.builder image derex/openedx/buildwheels)
 
-docker run -v "${PIP_CACHE}:/root/.cache/pip" -d --name derex.wheel.compiler --rm "$IMAGE" sleep 86400
+docker run -v /tmp/wheelhouse:/wheelhouse -v "${PIP_CACHE}:/root/.cache/pip" -d --name derex.wheel.compiler --rm "$IMAGE" sleep 86400
 
 docker exec derex.wheel.compiler git clone "$EDX_PLATFORM_REPOSITORY" --branch "${EDX_PLATFORM_VERSION}" --depth 1
 docker exec derex.wheel.compiler pip install wheel
@@ -14,9 +14,8 @@ docker exec derex.wheel.compiler sh -c "
 egrep -v '^-e|^git' edx-platform/requirements/edx/base.txt > base_external.txt
 echo '--find-links http://pypi.abzt.de/alpine-3.10' >> base_external.txt
 echo '--trusted-host pypi.abzt.de' >> base_external.txt
-mkdir /wheelhouse
 pip install numpy -c base_external.txt --find-links http://pypi.abzt.de/alpine-3.10
 pip wheel --wheel-dir=/wheelhouse -r base_external.txt --find-links http://pypi.abzt.de/alpine-3.10
 "
 
-#docker kill derex.wheel.compiler
+docker kill derex.wheel.compiler
