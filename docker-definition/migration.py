bb9a0f91b9b1a428cef845e8f26df31a4e54bf52
Silvio Tomatis <silviot@gmail.com>
2020-02-27 14:38:39 +0100

    Run mysql server on a different port to reduce chances of port clashing

--- docker-definition/migration.py
+++ docker-definition/migration.py
@@ -6,7 +6,17 @@ from ..common import *  # type: ignore
 from openedx.core.lib.derived import derive_settings
 
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.mysql", "NAME": "edxapp"}}
+# Use a custom mysql port to increase the probability of finding it free on a build machine.
+# buildkit seems to always use host networking mode, so it might clash
+# Attempts to add `--network=none` to the Dockerfile RUN directives proved fruiless
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "edxapp",
+        "HOST": "127.0.0.1",
+        "PORT": "3399",
+    }
+}
 
 # We need to define this, or we get an error
 XQUEUE_INTERFACE = {"django_auth": None, "url": None}
